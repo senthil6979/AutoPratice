ec19e470946c1085e5c686b82cb367f41353cca8
senthil6979 <46636469+senthil6979@users.noreply.github.com>
2019-01-20 20:03:35 -0800

    First Git commit

---
+++ src/main/java/com/qa/pages/LogoinPage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class LogoinPage {
+	
+	public void loginPage()
+	{
+		System.out.println("Login page...");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/LogoinTest.java
@@ -0,0 +1,12 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LogoinTest {
+	
+	@Test public void logoinTest()
+	{
+		System.out.println("Logoin Test...");
+	}
+
+}
